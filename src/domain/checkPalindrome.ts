9cbec1c13c92318512a043dbd57c90857c56c3c5
CM <corentin.mailler19@gmail.com>
2024-01-22 12:53:33 +0100

    feat: STEP 2 - add builder & language >> SUCCESS

--- src/domain/checkPalindrome.ts
+++ src/domain/checkPalindrome.ts
@@ -14,7 +14,7 @@ export class CheckPalindrome{
 
         let output = Formulation.BONJOUR + os.EOL + mirror + os.EOL 
         if (mirror == text){
-            output += ">> " + Formulation.BIEN_DIT + os.EOL;
+            output += ">> " + this._language.FindPalindrome() + os.EOL;
         }
 
         return output + Formulation.AU_REVOIR;
